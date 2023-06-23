2ea0c1fdf3ba9c2ac7cb2b75c8eb686ca9f20cb1
Hector Puch <hectorpuchortonoves@gmail.com>
2023-06-23 17:53:18 +0200

    Define functions of three parameters Exercise

---
+++ def_functions_three_parameters.py
@@ -0,0 +1,9 @@
+def address(street, city, postal_code, country):
+    print("Your address is: ", street, city, postal_code, country)
+
+s = input("Street: ")
+c = input("City: ")
+p_c = input("Postal Code: ")
+co = input("Country: ")
+
+address(s, c, p_c, co)
